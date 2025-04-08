ba570e90c59d1561d447475e2b053a5268d2f046
aloeless <aloegang067@gmail.com>
2025-04-09 00:02:57 +0300

    Add files via upload

---
+++ казик/config.py
@@ -0,0 +1,56 @@
+BOT_TOKEN = "7558351456:AAHc29GIFPkCkUbLyNTB_8A4d1ygt0i9AGg" 
+CRYPTOBOT_TOKEN = "366753:AAHdxHrIZbhy9sD9lNBBW4yx7oOANqdU3I2" 
+CHANNEL_ID = -1002615003001
+LOGS_ID = -4729732154
+ALL_ADMINS = [818661733] 
+ADMIN_IDS = "818661733,779278767" 
+ 
+NAME_PROJECT = "Dodep Cube" 
+ 
+BOT_LINK = "https://t.me/dodepcube_bot" 
+pinned_link = "http://t.me/send?start=IVskdtpQhYR5" 
+rules_link = "https://t.me/RuleKUMELOVDICE/2" 
+rule_link = "https://t.me/RuleKUMELOVDICE" 
+support_link = "https://t.me/pucky" 
+casino_link = "https://t.me/+DCacItyKWZFlZWRi" 
+  
+win_1_photo = "https://imgur.com/a/4aIoKCH" 
+win_2_photo = "https://imgur.com/a/08Gz9DL" 
+win_3_photo = "https://imgur.com/a/oda3g4N" 
+win_4_photo = "https://imgur.com/a/IQee1Tx" 
+win_5_photo = "https://imgur.com/a/igIHk6H" 
+win_6_photo = "https://imgur.com/a/Hbio46F" 
+win_7_photo = "https://imgur.com/a/2DZY5ha"
+ 
+
+ref_procent = 5 
+ 
+api_id = 6645910
+api_hash = "309d5b266bfa7dde5775c5039d0cd65f" 
+ 
+DATABASE_PATH = "alt_script.db" #не трогать 
+ 
+games_list = { 
+    'победа1': 'one', 
+    'победа2': 'two', 
+    'ничья': 'three', 
+    'меньше': 'four_one', 
+    'больше': 'four_two', 
+    'чёт': 'five_one', 
+    'чётное:': 'five_one', 
+    'чет': 'five_one', 
+    'чет': 'five_one', 
+    'нечёт': 'five_two', 
+    'нечётное': 'five_two', 
+    'нечет': 'five_two', 
+    'нечетное': 'five_two' 
+ 
+} 
+ 
+pay_x_amount = { 
+    "one": 1.8, 
+    "two": 1.8, 
+    "three": 3, 
+    "four": 1.7, 
+    "five": 1.7 
+}
